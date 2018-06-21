be56c994f61fa0415f560479643074aadfcba0ac
红力 <honglili@sohu-inc.com>
2018-06-21 10:55:49 +0800

    Initial Commit

---
+++ HLDownloadDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HLDownloadDemo
+//
+//  Created by LHL on 2018/6/21.
+//  Copyright © 2018 HL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HLDownloadDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HLDownloadDemo
+//
+//  Created by LHL on 2018/6/21.
+//  Copyright © 2018 HL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
