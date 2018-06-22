48a5c50a00a9e0b6258a497b2cdad419441e5192
lihongli <honglili@sohu-inc.com>
2018-06-23 00:17:26 +0800

    修改bug

---
+++ HLDownloadDemo/TableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TableViewCell.h
+//  HLDownloadDemo
+//
+//  Created by LiHongli on 2018/6/22.
+//  Copyright © 2018年 HL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewCell : UITableViewCell
+
+@end
---
+++ HLDownloadDemo/TableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.m
+//  HLDownloadDemo
+//
+//  Created by LiHongli on 2018/6/22.
+//  Copyright © 2018年 HL. All rights reserved.
+//
+
+#import "TableViewCell.h"
+
+@implementation TableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
